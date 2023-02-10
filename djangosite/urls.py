dd98adcd01e9b36531b3e16705e97030fde48640
monicapulipati09 <pulipatimonica@gmail.com>
2023-02-10 21:51:11 +0530

    -

--- djangosite/urls.py
+++ djangosite/urls.py
@@ -27,5 +27,5 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-admin.site.site_title = 'Monica Task MS'
-admin.site.site_header = 'Monica Task MS'
+admin.site.site_title = 'Articles Info'
+admin.site.site_header = 'Articles Info'
